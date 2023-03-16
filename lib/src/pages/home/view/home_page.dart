8f41aa7c6f3753a23c44358fe93337e38641effe
Javier Lopez <javierlopez19981705@gmail.com>
2023-03-16 17:55:07 -0600

    advances

--- lib/src/pages/home/view/home_page.dart
+++ lib/src/pages/home/view/home_page.dart
@@ -1,6 +1,16 @@
+import 'package:card_swiper/card_swiper.dart';
+import 'package:cities_repository/cities_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:weather_app/src/pages/home/view/widgets/current_weather_position/view/current_weather_position.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:weather_app/src/pages/home/cubit/home_cubit.dart';
+import 'package:weather_app/src/pages/weather_cities/cubit/weather_cities_cubit.dart';
+import 'package:weather_repository/weather_repository.dart';
+
+import '../../current_weather_position/cubit/current_weather_position_cubit.dart';
+import '../../current_weather_position/view/current_weather_position.dart';
+import '../../weather_cities/view/weather_cities_view.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({
@@ -12,30 +22,78 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CurrentWeatherPosition(position: position),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.red.withOpacity(.5),
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Inicio',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Buscar',
-          ),
-        ],
+    return BlocProvider(
+      create: (context) => HomeCubit(),
+      child: BlocBuilder<HomeCubit, HomeState>(
+        builder: (context, state) {
+          return MultiRepositoryProvider(
+            providers: [
+              RepositoryProvider(
+                create: (context) => CitiesRepository(
+                  prefs: SharedPreferences.getInstance(),
+                ),
+              ),
+              RepositoryProvider<WeatherRepository>(
+                create: (context) => WeatherRepository(),
+              ),
+            ],
+            child: MultiBlocProvider(
+              providers: [
+                BlocProvider<WeatherCitiesCubit>(
+                  create: (context) => WeatherCitiesCubit(
+                    citiesRepository: context.read<CitiesRepository>(),
+                  )..getCities(),
+                ),
+                BlocProvider<CurrentWeatherPositionCubit>(
+                  create: (context) => CurrentWeatherPositionCubit(
+                    weatherRepository: context.read<WeatherRepository>(),
+                    position: position,
+                    unit: state.units,
+                    unitStream:
+                        context.read<HomeCubit>().streamController.stream,
+                  )..getWeather(),
+                ),
+              ],
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Theme.of(context).colorScheme.secondary,
+                      Theme.of(context).primaryColor,
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                ),
+                child: Scaffold(
+                  backgroundColor: Colors.transparent,
+                  body: _HomeView(position: position),
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
 }
 
-// class _HomeView extends StatelessWidget {
-//   const _HomeView({super.key});
+class _HomeView extends StatelessWidget {
+  const _HomeView({required this.position});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Placeholder();
-//   }
-// }
+  final Position position;
+
+  @override
+  Widget build(BuildContext context) {
+    return Swiper.children(
+      viewportFraction: 0.8,
+      scale: 0.97,
+      loop: false,
+      physics: const BouncingScrollPhysics(),
+      children: [
+        CurrentWeatherPosition(position: position),
+        const WeatherCitiesView(),
+      ],
+    );
+  }
+}
